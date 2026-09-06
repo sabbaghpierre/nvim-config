f50775f7d846f729a75f8eb4bf52adb898e827fa
Pierre Sabbagh <pierresabbagh47@gmail.com>
2026-09-06 18:07:32 +0300

    feat: format jsx with prettier

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -491,6 +491,7 @@ require('conform').setup({
   formatters_by_ft = {
     lua = { 'stylua' },
     javascript = { 'prettier' },
+    javascriptreact = { 'prettier' },
     typescript = { 'prettier' },
     typescriptreact = { 'prettier' },
     go = { 'gofmt', 'goimports' },
