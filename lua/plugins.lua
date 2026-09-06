c2e0afe9bf27abcda328cb4a269cb30a93b37495
Pierre Sabbagh <pierresabbagh47@gmail.com>
2026-09-06 16:54:03 +0300

    fix: correct tD description

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -403,7 +403,7 @@ require('gitsigns').setup({
     map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
     map('n', '<leader>hD', function() gitsigns.diffthis('@') end, { desc = 'git [D]iff against last commit' })
     map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-    map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+    map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = 'Show git [D]eleted lines inline' })
   end,
 })
 
